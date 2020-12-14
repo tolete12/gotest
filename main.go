33eff1f16d8087b305b1939989a6eebe76af2179
Pablo <pablo@ldap.example.com>
2020-12-14 15:05:50 +0100

    added main and struct

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tolete12/gotest/structs"
+)
+
+func main() {
+	var p := Stats{Name: "algo"}
+	fmt.Printf(p.Name)
+	fmt.Printf("hello, world\n")
+}
---
+++ structs/structs.go
@@ -0,0 +1,6 @@
+package structs
+
+type Stats struct {
+	Name, Minutes                                        string
+	Points, Rebounds, Assists, Steals, Blocks, Turnovers uint8
+}
